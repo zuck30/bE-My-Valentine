be85d79b430aa316bc385340d6e764bddec1bab6
Timothy Zucker <155255765+zuck30@users.noreply.github.com>
2026-02-08 20:58:58 +0300

    footer component added

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p style={{ fontSize: '2.2rem', marginBottom: '20px' }}>
+        I love you more than yesterday,<br />but less than tomorrow, Shekha.
+      </p>
+      <p>Forever yours,<br /><strong>Ken ❤️</strong></p>
+      <p style={{ marginTop: '40px', fontSize: '1rem', opacity: '0.9' }}>
+        Made with all my love for the most beautiful woman in my world
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
